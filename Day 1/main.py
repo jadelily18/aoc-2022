99304a8c0b5688c098516501d8808ebc21d215d2
Jade Nash <jade.nashh18@gmail.com>
2022-12-07 13:51:02 -0600

    Day 1 Part 2

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,7 +1,11 @@
 def main():
     file_content = open("input.txt", "r").read()
+
     elf_inventories = file_content.split("\n\n")
-    print(max(get_totals(elf_inventories)))
+
+    calorie_totals = get_totals(elf_inventories)
+    calorie_totals.sort(reverse=True)
+    print(get_top_three_total(calorie_totals))
 
 
 def get_totals(inventories):
@@ -14,6 +18,14 @@ def get_totals(inventories):
                 calorie_total += int(i)
         totals.append(calorie_total)
     return totals
+
+
+def get_top_three_total(totals):
+    total = 0
+    for index, num in enumerate(totals):
+        if index == 3:
+            return total
+        total += num
 
 
 if __name__ == "__main__":
