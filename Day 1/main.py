e3d82a46ebad7331a2a575aa82c508865de2c6fd
Jade Nash <jade.nashh18@gmail.com>
2022-12-07 13:41:39 -0600

    Day 1 Part 1

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,1 +1,20 @@
-print("hiya")
+def main():
+    file_content = open("input.txt", "r").read()
+    elf_inventories = file_content.split("\n\n")
+    print(max(get_totals(elf_inventories)))
+
+
+def get_totals(inventories):
+    totals = []
+    for inv in inventories:
+        inv_list = inv.split("\n")
+        calorie_total = 0
+        for i in inv_list:
+            if i != '':
+                calorie_total += int(i)
+        totals.append(calorie_total)
+    return totals
+
+
+if __name__ == "__main__":
+    main()
